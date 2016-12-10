bd033aca9abb779d38e32944dce31c6cdfff2bdd
Phelps Williams <phelps@williamslabs.com>
2016-12-10 11:08:16 -0800

    Support for multithreading and scratch directories
    Rather than using a temp dir, also have the option to use a specific directory.
    Useful if disk space is limited in /tmp.  Multithreading also helps this backup
    tool.

--- backup.py
+++ backup.py
@@ -7,6 +7,7 @@ import tarfile
 import argparse
 import tempfile
 import gnupg
+from multiprocessing import Pool
 
 # TODO
 # - Verbosity
@@ -15,21 +16,13 @@ import gnupg
 # - Region setting
 
 
-def upload_file_list(filelist):
-    archive_name = 'glacier_archive_{}'.format(
-        datetime.utcnow().strftime('%Y%m%d%H%M%S'))
-    print(archive_name)
-
+def upload_file(archive, _file):
+    name = os.path.basename(_file)
     client = boto3.client('glacier')
-    response = client.create_vault(vaultName=archive_name,
-                                   accountId='-')
+    response = client.upload_archive(vaultName=archive,
+                                     archiveDescription=name,
+                                     body=_file)
     print(response)
-    for _file in filelist:
-        name = os.path.basename(_file)
-        response = client.upload_archive(vaultName=archive_name,
-                                         archiveDescription=name,
-                                         body=_file)
-        print(response)
     hostname = open('/etc/hostname').read().rstrip()
     timestamp = datetime.utcnow().isoformat()
     client.add_tags_to_vault(vaultName=archive_name,
@@ -37,67 +30,77 @@ def upload_file_list(filelist):
                                    'timestamp': timestamp})
 
 
-def create_backups(directories, unencrypted, cryptokey, dry_run):
-    with tempfile.TemporaryDirectory() as _dir:
-        files_to_upload = []
-        for bkupdir in directories:
-            abspath = os.path.abspath(bkupdir)
-            bkupfile = os.path.basename(abspath) + '.tar.xz'
-            absbkfile = os.path.join(_dir, bkupfile)
+def create_backup(args):
+    path, scratch, cryptokey, archive, dry_run = args
 
-            if not os.path.isdir(abspath):
-                print('Unknown directory {}'.format(abspath))
-                continue
+    abspath = os.path.abspath(path)
+    bkupfile = os.path.basename(abspath) + '.tar.xz'
+    absbkfile = os.path.join(scratch, bkupfile)
 
-            # Create compressed tar file of the target directory
-            tar = tarfile.open(absbkfile, "w:xz")
-            tar.add(abspath,
-                    arcname=os.path.basename(abspath),
-                    recursive=True)
-            tar.close()
+    if not os.path.isdir(abspath):
+        print('Unknown directory {}'.format(abspath))
+        return
 
-            if not unencrypted:
-                # Encrypt archive file
-                abscryptfile = '{}.gpg'.format(absbkfile)
-                gpg = gnupg.GPG(homedir=os.path.join(_dir, 'gnupg'))
-                gpg.recv_keys(cryptokey, keyserver='hkp://pgp.mit.edu')
-                with open(absbkfile, 'rb') as f:
-                    status = gpg.encrypt(
-                        f, cryptokey,
-                        output=abscryptfile,
-                        armor=False)
-                    print(abscryptfile)
-                    if not status.ok:
-                        print('Encryption error')
-                        continue
-                os.remove(absbkfile)
+    # Create compressed tar file of the target directory
+    tar = tarfile.open(absbkfile, "w:xz")
+    tar.add(abspath,
+	    arcname=os.path.basename(abspath),
+	    recursive=True)
+    tar.close()
 
-            # Add appropriate file to list of files to upload
-            if not unencrypted:
-                files_to_upload.append(abscryptfile)
-            else:
-                files_to_upload.append(absbkfile)
+    # Encrypt archive file
+    abscryptfile = '{}.gpg'.format(absbkfile)
+    gpg = gnupg.GPG(homedir=os.path.join(scratch, 'gnupg'))
+    gpg.recv_keys(cryptokey, keyserver='hkp://pgp.mit.edu')
+    with open(absbkfile, 'rb') as f:
+        status = gpg.encrypt(
+            f, cryptokey,
+            output=abscryptfile,
+            armor=False)
+        print(abscryptfile)
+        if not status.ok:
+            print('Encryption error')
+            return
+    os.remove(absbkfile)
 
-        # Upload archive to glacier
-        print(files_to_upload)
-        if not dry_run:
-            upload_file_list(files_to_upload)
+    if not dry_run:
+        upload_file(archive, abscryptfile)
+    os.remove(abscryptfile)
+    
+
+def create_backups(directories, scratch, cryptokey, threads, dry_run):
+    archive = 'glacier_archive_{}'.format(
+        datetime.utcnow().strftime('%Y%m%d%H%M%S'))
+    client = boto3.client('glacier')
+    response = client.create_vault(vaultName=archive,
+                                   accountId='-')
+    if scratch:
+        process_args = [(x, scratch, cryptokey, archive, dry_run) for x in directories]
+        pool = Pool(threads)
+        pool.map(create_backup, process_args)
+    else:
+        with tempfile.TemporaryDirectory() as _dir:
+            process_args = [(x, _dir, cryptokey, archive, dry_run) for x in directories]
+            pool = Pool(threads)
+            pool.map(create_backup, process_args)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('directories', metavar='N', type=str, nargs='+',
                         help='List of directories to backup.')
+    parser.add_argument('--scratch', type=str)
     parser.add_argument('--dry-run', action='store_true')
-    parser.add_argument('--unencrypted', action='store_true')
+    parser.add_argument('--threads', type=int, default=1)
     parser.add_argument('--cryptokey',
                         type=str,
-                        default='phelps@williamslabs.com',
                         help='Encryption key to encrypt for.')
     args = parser.parse_args()
     start_time = datetime.utcnow()
     create_backups(args.directories,
-                   args.unencrypted,
+                   args.scratch,
                    args.cryptokey,
+                   args.threads,
                    args.dry_run)
     end_time = datetime.utcnow()
     print('Run duration {}'.format(end_time - start_time))
